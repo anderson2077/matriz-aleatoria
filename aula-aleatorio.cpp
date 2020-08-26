7487fc19bf47a90792f5620493539f69660f6c1d
Anderson <andersoncleyson@protonmail.com>
2020-08-25 23:30:51 -0500

    Add matriz-aleatoria.cpp

---
+++ aula-aleatorio.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+int main(){
+	int a, i ,j, b, c, aleatorio;
+	
+	srand((unsigned)time(NULL));
+		
+	printf("======GERADOR DE MATRIZES======\n");
+	printf("\n");
+	printf("Digite o tamanho da matriz (axb): ");
+	scanf("%d %d", &a, &b);
+	int matriz[a][b];
+		for(i = 0; i < a; i ++){
+			for(j = 0; j < b; j++){
+				aleatorio = rand() % 9; //Tem a função de gerar n�meros pseudo-aleatórios
+				matriz[i][j] = aleatorio; //Preenche a matriz com valores aleatórios
+				printf("%d ", matriz[i][j]);
+				if(j == (b-1)){
+					printf("\n");
+				}
+				
+			}	
+		}
+		
+		
+		printf("------------------------------------------------\n");
+		
+	
+	
+system("PAUSE");
+
+}
