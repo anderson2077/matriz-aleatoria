b126dd2909494e3274fd425689af0554e31bc3a0
Anderson <andersoncleyson@protonmail.com>
2020-08-26 00:36:14 -0500

    intervalo dos numeros

--- matriz-aleatoria.cpp
+++ matriz-aleatoria.cpp
@@ -12,10 +12,12 @@ int main(){
 	printf("\n");
 	printf("Digite o tamanho da matriz (axb): ");
 	scanf("%d %d", &a, &b);
+	printf("Digite o intervalo dos números aleatórios: ")
+	scanf("%d", &c);
 	int matriz[a][b];
 		for(i = 0; i < a; i ++){
 			for(j = 0; j < b; j++){
-				aleatorio = rand() % 9; //Tem a função de gerar números pseudo-aleatórios
+				aleatorio = rand() % c; //Tem a função de gerar números pseudo-aleatórios
 				matriz[i][j] = aleatorio; //Preenche a matriz com números aleatórios			
 				printf("%d ", matriz[i][j]);
 				if(j == (b-1)){
